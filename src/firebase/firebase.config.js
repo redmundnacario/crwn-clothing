a39b838905a0193eb8a898e45ff10a660c23f039
redmundnacario <redmund.nacario@gmail.com>
2020-12-17 22:23:06 +0800

    heroku deployment

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig  =  {
+    apiKey: "AIzaSyDdWq6IoCxuW9qcaHhV3AIIDCPxMXux0oE",
+    authDomain: "crwn-clothing-21424.firebaseapp.com",
+    databaseURL: "https://crwn-clothing-21424.firebaseio.com",
+    projectId: "crwn-clothing-21424",
+    storageBucket: "crwn-clothing-21424.appspot.com",
+    messagingSenderId: "476766285357",
+    appId: "1:476766285357:web:031aea3a5d5054e10792a4",
+    measurementId: "G-W45QHNF0CV"
+  };
+
+export default firebaseConfig;
