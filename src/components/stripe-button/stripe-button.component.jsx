f4e4ae81b335b2b2e18154263867c8c7f7e02391
redmundnacario <redmund.nacario@gmail.com>
2020-12-18 00:06:00 +0800

    organized keys

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_51Hz7QHHUMa7i9AVOoEClsrMNqYX2LTSmnPuzz9DLMYeZgwenxJ7bIBtLS5qPgsTadJQwbW0zWrppxhBx9wRF8vUI00xo5ikLvc';
+  const publishableKey = process.env.REACT_APP_stripe_publishableKey;
 
   const onToken = token => {
     console.log(token);
